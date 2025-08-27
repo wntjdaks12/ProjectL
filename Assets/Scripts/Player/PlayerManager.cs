ccbe5688f2a14d33e2d0e6db8e91247fef652b47
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2025-08-28 02:37:06 +0900

    플레이어 매니저 추가

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] private List<Item> items;
+
+    private void Awake()
+    {
+        items = new List<Item>();
+    }
+
+    private void Start()
+    {
+        
+    }
+}
