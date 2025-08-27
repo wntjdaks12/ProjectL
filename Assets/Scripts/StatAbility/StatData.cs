c653df9c803b054a508cabdf3fe0cf666a91e626
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2025-08-28 03:00:27 +0900

    스탯 추가

---
+++ Assets/Scripts/StatAbility/StatData.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class StatData 
+{
+    public List<Stat> Stats { get; set; }
+
+    public float GetTotalStatValue(Stat.StatTypes statType)
+    {
+        return Stats.Where(x => x.StatType == statType).Sum(x => x.Value);
+    }
+
+    public void UpdateStatValue(Stat.StatTypes statType, float value)
+    {
+        var stat = Stats.Where(x => x.StatType == statType).FirstOrDefault();
+
+        stat.Value += value;
+    }
+}
