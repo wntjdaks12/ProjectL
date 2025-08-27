3e50bd9d8727a3618cfd84fe46ad6b6eafbee246
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2025-08-28 02:25:25 +0900

    아이템 데이터 구조 추가

---
+++ Assets/Scripts/Data/Item.cs
@@ -0,0 +1,39 @@
+using Newtonsoft.Json;
+using System;
+
+public enum ItemType
+{
+    Equipment,      // 장비
+    Consumable,     // 소비
+    Misc            // 기타
+}
+
+public class Item : ICloneable
+{
+    [JsonProperty] private int id;
+    [JsonProperty] private ItemType itemType;
+    [JsonProperty] private int count;
+
+    public int Id => id;
+    public ItemType ItemType => itemType;
+    public int Count => count;
+
+    // 아이템 추가
+    public void AddItem(int count)
+    {
+        this.count += count;
+    }
+
+    // 아이템 사용
+    public void ConsumeItem(int count)
+    {
+        var curCount = this.count - count;
+
+        this.count = Math.Clamp(curCount, 0, curCount);
+    }
+
+    public object Clone()
+    {
+        return MemberwiseClone();
+    }
+}
