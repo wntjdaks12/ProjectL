bd6cf2e9847caf47a4faf9eb6b6f5296e2a8d0e7
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2025-08-28 05:53:58 +0900

    스탯 추가

--- Assets/Scripts/Actor/Character/Character.cs
+++ Assets/Scripts/Actor/Character/Character.cs
@@ -1,3 +1,25 @@
+using UnityEngine;
+
 public class Character : Actor
 {
+    public StatData StatData { get; set; }
+    public StatAbility StatAbility { get; set; }
+
+    public override void Init(Transform transform = null)
+    {
+        base.Init(transform);
+        // 스탯 관련 추가 ----------------------------------------------------------------------------------------
+        StatAbility = new StatAbility();
+
+
+        // 메인 스탯 추가
+        StatAbility.AddStatData(StatAbility.StatInfo.StatDataType.Main, StatData);
+
+        StatAbility.CurrentSpeed = StatAbility.MaxSpeed;
+        StatAbility.CurrentHp = StatAbility.MaxHp;
+        StatAbility.CurrentMp = StatAbility.MaxMp;
+        StatAbility.CurrentStamina = StatAbility.MaxStamina;
+        StatAbility.CurrentBasicAttackRange = StatAbility.BasicAttackRange;
+        // --------------------------------------------------------------------------------------------------------
+    }
 }
